39fa3bebe7ae97943280725e21e9ca032f92122e
Ugo Anayo <uggoprince@gmail.com>
2022-09-09 11:18:18 +0100

    code base setup complete

---
+++ pages/index.tsx
@@ -0,0 +1,27 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Advertise your car on automart" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
